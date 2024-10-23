34f7f83c2e8266908916d216ea0c4ada82f5ed94
shotoyaar <anirudhsridhar1064@gmail.com>
2024-10-23 22:08:46 +0530

    feat: initial react native setup

---
+++ app/index.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TextInput
+        style={styles.input}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 12,
+  },
+});
