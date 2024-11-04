ea7251f2faa884d4980d5c5d8befcc61e5add6dc
shotoyaar <anirudhsridhar1064@gmail.com>
2024-11-05 01:07:53 +0530

    FEAT: added non-persistent states to allow users to add medicines.

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,29 +1,197 @@
 import Medicine from '@/components/Medicine';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { KeyboardAvoidingView, Modal, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+type Med = {
+    medName: string;
+    count: number;
+};
+
+const initialMedState: Med = {
+    medName: "",
+    count: 0
+};
 
 export default function HomeScreen() {
+
+    const [newMed, setNewMed] = useState<Med>(initialMedState);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [addedMeds, setAddedMeds] = useState<Med[]>([]);
+
+    const handleMedicineAddition = () => {
+        console.log(newMed);
+
+        if (newMed.medName.trim() === '' && newMed.count <= 0) {
+            return;
+        }
+
+        setAddedMeds([...addedMeds, newMed]);
+        setNewMed(initialMedState);
+        setModalVisible(!modalVisible);
+    };
+
+    const toggleModal = () => {
+        setModalVisible(!modalVisible);
+        setNewMed(initialMedState);
+    };
+
     return (
-        <View style = { styles.overallContainer }>
-            <Text style = { styles.initBox }>your medicines</Text>
-            <Medicine medName="Paracetamol" count={10} />
-            <Medicine medName="Anthraprazole" count={15} />
+        <View style={styles.overallContainer}>
+            <View style={styles.medsWrapper}>
+                <Text style={styles.initBox}>your medicines</Text>
+                <View style={styles.medComponents}>
+                    {
+                        addedMeds.map((med, i) => {
+                            return <Medicine
+                                key={i}
+                                medName={med.medName}
+                                count={med.count}
+                            />
+                        })
+                    }
+                </View>
+            </View>
+
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={toggleModal}
+            >
+                <KeyboardAvoidingView
+                    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                    style={styles.modal}
+                >
+                    <TextInput
+                        style={styles.input}
+                        placeholder={`Add a new medicine`}
+                        value={newMed.medName}
+                        onChangeText={text => setNewMed(prev => ({ ...prev, medName: text }))}
+                    />
+
+                    <TextInput
+                        style={styles.input}
+                        placeholder={`How much medicine`}
+                        keyboardType="numeric"
+                        value={newMed.count.toString()}
+                        onChangeText={text => {
+                            const count = text === '' ? 0 : parseInt(text);
+                            setNewMed(prev => ({ ...prev, count }));
+                        }}
+                    />
+
+                    <View style={styles.modalOperations}>
+                        <TouchableOpacity
+                            onPress={() => { handleMedicineAddition() }}
+                            style={styles.addMedicine}
+                        >
+                            <Text>Add</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            onPress={() => { toggleModal() }}
+                            style={styles.closeModal}
+                        >
+                            <Text>Close</Text>
+                        </TouchableOpacity>
+                    </View>
+                </KeyboardAvoidingView>
+            </Modal>
+
+            <TouchableOpacity
+                style={styles.addWrapper}
+                onPress={() => { toggleModal() }}
+            >
+                <View style={styles.addButton}>
+                    <Text style={styles.addText}>+</Text>
+                </View>
+            </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    initBox : {
+    initBox: {
         color: "white",
         fontSize: 30,
         fontWeight: "bold",
         textTransform: "capitalize",
     },
-
-    overallContainer : {
+    overallContainer: {
         flex: 1,
-        marginTop: 40,
-        margin: 10,
-        paddingTop: 60,
+    },
+    medsWrapper: {
+        paddingTop: 80,
         paddingHorizontal: 20,
+    },
+    medComponents: {
+        marginTop: 20,
+    },
+    keyboard: {
+
+    },
+    input: {
+        padding: 20,
+        backgroundColor: "white",
+        borderColor: "black",
+        borderWidth: 1,
+        marginBottom: 10,
+        borderRadius: 20,
+
+    },
+    addWrapper: {
+        position: "absolute",
+        width: "100%",
+        height: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        bottom: 50,
+    },
+    addButton: {
+        position: "absolute",
+        right: 30,
+        borderRadius: 60,
+        height: 60,
+        width: 60,
+        backgroundColor: "white",
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "white",
+    },
+    addText: {
+        color: "black",
+        fontSize: 20,
+    },
+    modal: {
+        position: "absolute",
+        flexDirection: "column",
+        height: "70%",
+        width: "100%",
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        bottom: 0,
+    },
+    addMedicine: {
+        backgroundColor: "green",
+        padding: 20,
+        borderRadius: 20,
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    closeModal: {
+        backgroundColor: "red",
+        padding: 20,
+        borderRadius: 20,
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    modalOperations: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 30,
     }
 });
--- frontend/components/Medicine.tsx
+++ frontend/components/Medicine.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Alert, StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
+import { Alert, Modal, Pressable, StyleSheet, Text, TouchableNativeFeedback, View } from 'react-native';
 
 const Medicine = ({
     medName,
@@ -10,41 +10,82 @@ const Medicine = ({
 }) => {
 
     const [currCount, setCurrCount] = useState(count);
+    const [modalVisible, setModalVisible] = useState(false);
 
-    const handleClick = () => {
+    const handleSubtraction = () => {
         if (currCount > 0) {
             console.log(`You have ${count} ${medName} left`);
             setCurrCount(currCount - 1);
 
             if (currCount - 1 === 0) {
-                Alert.alert("Alert",`Please restock ${medName}`, [{ text: "OK" }]);
+                Alert.alert("Alert", `Please restock ${medName}`, [{ text: "OK" }]);
             }
-        } else {
-            setCurrCount(currCount + 1);
         }
     };
 
-    return (
-        <TouchableNativeFeedback onPress = { handleClick }>
-            <View style = { styles.medContainer }>
-                <View style = { styles.medicinePill }>
-                    <View style = { styles.pill1 }></View>
-                    <View style = { styles.pill2 }></View>
-                </View>
+    const handleAddition = () => {
+        setCurrCount(currCount + 1);
+        console.log(`You have ${count} ${medName} now`);
+    };
 
-                <Text style = { styles.medTextStyle }>{medName}</Text>
-                <Text style = { styles.medCountStyle }>{currCount}</Text>
-            </View>
-        </TouchableNativeFeedback>
+    return (
+        <View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert('modal has been closed.');
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>{medName}</Text>
+                        <View style={styles.valueButtons}>
+                            <Pressable
+                                style={styles.increase}
+                                onPress={handleAddition}>
+                                <Text style={styles.textStyle}>Add {medName}</Text>
+                            </Pressable>
+
+                            <Pressable
+                                style={styles.decrease}
+                                onPress={handleSubtraction}>
+                                <Text style={styles.textStyle}>Remove {medName}</Text>
+                            </Pressable>
+                        </View>
+                        <Pressable
+                            style={styles.closeButton}
+                            onPress={() => setModalVisible(!modalVisible)}
+                        >
+                            <Text style={styles.textStyle}>Close</Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </Modal>
+
+            <TouchableNativeFeedback onPress={() => { setModalVisible(true) }}>
+                <View style={styles.medContainer}>
+                    <View style={styles.medicinePill}>
+                        <View style={styles.pill1}></View>
+                        <View style={styles.pill2}></View>
+                    </View>
+
+                    <Text style={styles.medTextStyle}>{medName}</Text>
+                    <Text style={styles.medCountStyle}>{currCount}</Text>
+                </View>
+            </TouchableNativeFeedback>
+        </View>
     );
 };
 
 export default Medicine;
 
 const styles = StyleSheet.create({
-    medContainer : {
+    medContainer: {
         flexDirection: "row",
-        backgroundColor: "grey",
+        backgroundColor: "white",
         marginVertical: 5,
         marginTop: 20,
         paddingVertical: 10,
@@ -53,40 +94,83 @@ const styles = StyleSheet.create({
         borderColor: "white",
         borderWidth: 1,
     },
-
     medicinePill: {
         flexDirection: "column",
         height: 25,
         width: 15,
         marginRight: 10,
         borderRadius: 25,
-        transform : [{ rotate: "45deg" }],
+        transform: [{ rotate: "45deg" }],
         overflow: "hidden",
-        borderColor: "white",
-        borderWidth: 1,
     },
-
-    pill1 : {
+    pill1: {
         backgroundColor: "blue",
         flex: 1,
     },
-
-    pill2 : {
+    pill2: {
         backgroundColor: "red",
         flex: 1,
     },
-
-    medTextStyle : {
-        color: "white",
+    medTextStyle: {
+        color: "black",
         fontSize: 20,
         width: "80%",
         paddingLeft: 10,
     },
-
-    medCountStyle : {
-        color: "white",
+    medCountStyle: {
+        color: "black",
         fontSize: 20,
         textAlign: "right",
         width: "10%",
     },
+    increase: {
+        backgroundColor: "green",
+        padding: 25,
+        borderRadius: 10,
+        margin: 10,
+        width: "50%",
+    },
+    decrease: {
+        backgroundColor: "red",
+        padding: 25,
+        borderRadius: 10,
+        margin: 10,
+        width: "50%"
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        padding: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: "100%",
+    },
+    modalText: {
+        marginBottom: 15,
+        width: "100%",
+        textAlign: 'center',
+    },
+    textStyle: {
+        color: "white",
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    valueButtons: {
+        flexDirection: "row",
+        marginTop: 20,
+    },
+    closeButton: {
+        backgroundColor: "red",
+        padding: 10,
+        borderRadius: 10,
+        width: "20%"
+    }
 });
