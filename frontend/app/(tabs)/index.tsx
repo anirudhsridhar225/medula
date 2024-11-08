38fcc7173a4c2c76a77e4660fb5450ea6493c47d
shotoyaar <anirudhsridhar1064@gmail.com>
2024-11-08 23:29:33 +0530

    chore: updated documentation

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,274 +1,274 @@
 import React, { useState, useEffect } from "react";
 import {
-  KeyboardAvoidingView,
-  Modal,
-  Platform,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
+	KeyboardAvoidingView,
+	Modal,
+	Platform,
+	StyleSheet,
+	Text,
+	TextInput,
+	TouchableOpacity,
+	View,
 } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Medicine from "@/components/Medicine";
 
 type Med = {
-  medName: string;
-  count: number;
+	medName: string;
+	count: number;
 };
 
 const initialMedState: Med = {
-  medName: "",
-  count: 0,
+	medName: "",
+	count: 0,
 };
 
 export default function HomeScreen() {
-  const [newMed, setNewMed] = useState<Med>(initialMedState);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [addedMeds, setAddedMeds] = useState<Med[]>([]);
+	const [newMed, setNewMed] = useState<Med>(initialMedState);
+	const [modalVisible, setModalVisible] = useState(false);
+	const [addedMeds, setAddedMeds] = useState<Med[]>([]);
 
-  useEffect(() => {
-    loadMedicineData();
-  }, []);
+	useEffect(() => {
+		loadMedicineData();
+	}, []);
 
-  useEffect(() => {
-    saveMedicineData();
-  }, [addedMeds]);
+	useEffect(() => {
+		saveMedicineData();
+	}, [addedMeds]);
 
-  const loadMedicineData = async () => {
-    try {
-      const jsonValue = await AsyncStorage.getItem("medicines");
-      if (jsonValue !== null) {
-        setAddedMeds(JSON.parse(jsonValue));
-      } else {
-        console.log("No medicine data found in AsyncStorage");
-      }
-    } catch (e) {
-      console.error("Error loading medicine data:", e);
-    }
-  };
+	const loadMedicineData = async () => {
+		try {
+			const jsonValue = await AsyncStorage.getItem("medicines");
+			if (jsonValue !== null) {
+				setAddedMeds(JSON.parse(jsonValue));
+			} else {
+				console.log("No medicine data found in AsyncStorage");
+			}
+		} catch (e) {
+			console.error("Error loading medicine data:", e);
+		}
+	};
 
-  const saveMedicineData = async () => {
-    try {
-      await AsyncStorage.setItem("medicines", JSON.stringify(addedMeds));
-    } catch (e) {
-      console.error("Error saving medicine data:", e);
-    }
-  };
+	const saveMedicineData = async () => {
+		try {
+			await AsyncStorage.setItem("medicines", JSON.stringify(addedMeds));
+		} catch (e) {
+			console.error("Error saving medicine data:", e);
+		}
+	};
 
-  const handleMedicineAddition = () => {
-    if (newMed.medName.trim() === "" || newMed.count <= 0) {
-      return;
-    }
+	const handleMedicineAddition = () => {
+		if (newMed.medName.trim() === "" || newMed.count <= 0) {
+			return;
+		}
 
-    setAddedMeds([...addedMeds, newMed]);
-    setNewMed(initialMedState);
-    setModalVisible(!modalVisible);
-  };
+		setAddedMeds([...addedMeds, newMed]);
+		setNewMed(initialMedState);
+		setModalVisible(!modalVisible);
+	};
 
-  const handleMedicineDeletion = async (medName: string) => {
-    try {
-      const updatedMeds = addedMeds.filter((med) => med.medName !== medName);
-      setAddedMeds(updatedMeds);
-    } catch (e) {
-      console.error("Error deleting medicine:", e);
-    }
-  };
+	const handleMedicineDeletion = async (medName: string) => {
+		try {
+			const updatedMeds = addedMeds.filter((med) => med.medName !== medName);
+			setAddedMeds(updatedMeds);
+		} catch (e) {
+			console.error("Error deleting medicine:", e);
+		}
+	};
 
-  const handleMedicineUpdation = async (medName: string, newCount: number) => {
-    try {
-      const updatedMeds = addedMeds.map((med) => {
-        if (med.medName === medName) {
-          return { ...med, count: newCount };
-        }
-        return med;
-      });
-      setAddedMeds(updatedMeds);
-      await saveMedicineData();
-    } catch (e) {
-      console.error("Error updating medicine:", e);
-    }
-  };
+	const handleMedicineUpdation = async (medName: string, newCount: number) => {
+		try {
+			const updatedMeds = addedMeds.map((med) => {
+				if (med.medName === medName) {
+					return { ...med, count: newCount };
+				}
+				return med;
+			});
+			setAddedMeds(updatedMeds);
+			await saveMedicineData();
+		} catch (e) {
+			console.error("Error updating medicine:", e);
+		}
+	};
 
-  const toggleModal = () => {
-    setModalVisible(!modalVisible);
-    setNewMed(initialMedState);
-  };
+	const toggleModal = () => {
+		setModalVisible(!modalVisible);
+		setNewMed(initialMedState);
+	};
 
-  return (
-    <View style={styles.overallContainer}>
-      <View style={styles.medsWrapper}>
-        <Text style={styles.initBox}>your medicines</Text>
-        <View style={styles.medComponents}>
-          {addedMeds.map((med, i) => (
-            <Medicine 
-              key={i} 
-              medName={med.medName} 
-              count={med.count} 
-              onDelete={handleMedicineDeletion} 
-              onUpdate={handleMedicineUpdation}
-            />
-          ))}
-        </View>
-      </View>
+	return (
+		<View style={styles.overallContainer}>
+			<View style={styles.medsWrapper}>
+				<Text style={styles.initBox}>your medicines</Text>
+				<View style={styles.medComponents}>
+					{addedMeds.map((med, i) => (
+						<Medicine
+							key={i}
+							medName={med.medName}
+							count={med.count}
+							onDelete={handleMedicineDeletion}
+							onUpdate={handleMedicineUpdation}
+						/>
+					))}
+				</View>
+			</View>
 
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={toggleModal}
-      >
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-          style={styles.modal}
-        >
-          <Text style={styles.modalHeading}>Add a new medicine</Text>
-          <Text style={styles.modalSubHeading}>Medicine Name:</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="Add a new medicine"
-            value={newMed.medName}
-            onChangeText={(text) =>
-              setNewMed((prev) => ({ ...prev, medName: text }))
-            }
-          />
+			<Modal
+				animationType="slide"
+				transparent={true}
+				visible={modalVisible}
+				onRequestClose={toggleModal}
+			>
+				<KeyboardAvoidingView
+					behavior={Platform.OS === "ios" ? "padding" : "height"}
+					style={styles.modal}
+				>
+					<Text style={styles.modalHeading}>Add a new medicine</Text>
+					<Text style={styles.modalSubHeading}>Medicine Name:</Text>
+					<TextInput
+						style={styles.input}
+						placeholder="Add a new medicine"
+						value={newMed.medName}
+						onChangeText={(text) =>
+							setNewMed((prev) => ({ ...prev, medName: text }))
+						}
+					/>
 
-          <Text style={styles.modalSubHeading}>Medicine Count:</Text>
-          <TextInput
-            style={styles.input}
-            placeholder="How much medicine"
-            keyboardType="numeric"
-            value={newMed.count.toString()}
-            onChangeText={(text) => {
-              const count = text === "" ? 0 : parseInt(text);
-              setNewMed((prev) => ({ ...prev, count }));
-            }}
-          />
+					<Text style={styles.modalSubHeading}>Medicine Count:</Text>
+					<TextInput
+						style={styles.input}
+						placeholder="How much medicine"
+						keyboardType="numeric"
+						value={newMed.count.toString()}
+						onChangeText={(text) => {
+							const count = text === "" ? 0 : parseInt(text);
+							setNewMed((prev) => ({ ...prev, count }));
+						}}
+					/>
 
-          <View style={styles.modalOperations}>
-            <TouchableOpacity
-              onPress={handleMedicineAddition}
-              style={styles.addMedicine}
-            >
-              <Text style={styles.addText}>Add</Text>
-            </TouchableOpacity>
+					<View style={styles.modalOperations}>
+						<TouchableOpacity
+							onPress={handleMedicineAddition}
+							style={styles.addMedicine}
+						>
+							<Text style={styles.addText}>Add</Text>
+						</TouchableOpacity>
 
-            <TouchableOpacity onPress={toggleModal} style={styles.closeModal}>
-              <Text style={styles.addText}>Close</Text>
-            </TouchableOpacity>
-          </View>
-        </KeyboardAvoidingView>
-      </Modal>
+						<TouchableOpacity onPress={toggleModal} style={styles.closeModal}>
+							<Text style={styles.addText}>Close</Text>
+						</TouchableOpacity>
+					</View>
+				</KeyboardAvoidingView>
+			</Modal>
 
-      <TouchableOpacity
-        style={styles.addWrapper}
-        onPress={() => {
-          toggleModal();
-        }}
-      >
-        <View style={styles.addButton}>
-          <Text style={styles.addButtonText}>+</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
-  );
+			<TouchableOpacity
+				style={styles.addWrapper}
+				onPress={() => {
+					toggleModal();
+				}}
+			>
+				<View style={styles.addButton}>
+					<Text style={styles.addButtonText}>+</Text>
+				</View>
+			</TouchableOpacity>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  initBox: {
-    color: "white",
-    fontSize: 30,
-    fontWeight: "bold",
-    textTransform: "capitalize",
-  },
-  overallContainer: {
-    flex: 1,
-  },
-  medsWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
-  },
-  medComponents: {
-    marginTop: 20,
-  },
-  input: {
-    padding: 20,
-    backgroundColor: "white",
-    borderColor: "black",
-    borderWidth: 1,
-    marginBottom: 10,
-    borderRadius: 20,
-  },
-  addWrapper: {
-    position: "absolute",
-    width: "100%",
-    height: 10,
-    alignItems: "center",
-    justifyContent: "center",
-    bottom: 50,
-  },
-  addButton: {
-    position: "absolute",
-    right: 30,
-    borderRadius: 60,
-    height: 60,
-    width: 60,
-    backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center",
-    borderWidth: 1,
-    borderColor: "white",
-  },
-  addText: {
-    color: "white",
-    fontSize: 20,
-    paddingHorizontal: 10,
-  },
-  addButtonText: {
-    color: "black",
-    fontSize: 20,
-  },
-  modal: {
-    position: "absolute",
-    flexDirection: "column",
-    height: "70%",
-    width: "100%",
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    bottom: 0,
-  },
-  addMedicine: {
-    backgroundColor: "green",
-    padding: 20,
-    borderRadius: 20,
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  closeModal: {
-    backgroundColor: "red",
-    padding: 20,
-    borderRadius: 20,
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  modalOperations: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    padding: 30,
-  },
-  modalHeading : {
-    color: "black",
-    fontSize: 30,
-    fontWeight: "bold",
-    marginBottom: 20,
-    textAlign: "center",
-  },
-  modalSubHeading: {
-    color: "black",
-    marginVertical: 5,
-    fontSize: 20,
-  },
+	initBox: {
+		color: "white",
+		fontSize: 30,
+		fontWeight: "bold",
+		textTransform: "capitalize",
+	},
+	overallContainer: {
+		flex: 1,
+	},
+	medsWrapper: {
+		paddingTop: 80,
+		paddingHorizontal: 20,
+	},
+	medComponents: {
+		marginTop: 20,
+	},
+	input: {
+		padding: 20,
+		backgroundColor: "white",
+		borderColor: "black",
+		borderWidth: 1,
+		marginBottom: 10,
+		borderRadius: 20,
+	},
+	addWrapper: {
+		position: "absolute",
+		width: "100%",
+		height: 10,
+		alignItems: "center",
+		justifyContent: "center",
+		bottom: 50,
+	},
+	addButton: {
+		position: "absolute",
+		right: 30,
+		borderRadius: 60,
+		height: 60,
+		width: 60,
+		backgroundColor: "white",
+		justifyContent: "center",
+		alignItems: "center",
+		borderWidth: 1,
+		borderColor: "white",
+	},
+	addText: {
+		color: "white",
+		fontSize: 20,
+		paddingHorizontal: 10,
+	},
+	addButtonText: {
+		color: "black",
+		fontSize: 20,
+	},
+	modal: {
+		position: "absolute",
+		flexDirection: "column",
+		height: "70%",
+		width: "100%",
+		backgroundColor: "white",
+		borderRadius: 20,
+		padding: 35,
+		bottom: 0,
+	},
+	addMedicine: {
+		backgroundColor: "green",
+		padding: 20,
+		borderRadius: 20,
+		textAlign: "center",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	closeModal: {
+		backgroundColor: "red",
+		padding: 20,
+		borderRadius: 20,
+		textAlign: "center",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	modalOperations: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		padding: 30,
+	},
+	modalHeading: {
+		color: "black",
+		fontSize: 30,
+		fontWeight: "bold",
+		marginBottom: 20,
+		textAlign: "center",
+	},
+	modalSubHeading: {
+		color: "black",
+		marginVertical: 5,
+		fontSize: 20,
+	},
 });
--- frontend/components/Medicine.tsx
+++ frontend/components/Medicine.tsx
@@ -1,206 +1,213 @@
 import { useState } from "react";
 import {
-  Alert,
-  Modal,
-  Pressable,
-  StyleSheet,
-  Text,
-  TouchableNativeFeedback,
-  View,
+	Alert,
+	Modal,
+	Pressable,
+	StyleSheet,
+	Text,
+	TouchableNativeFeedback,
+	View,
 } from "react-native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+
+/**
+ * Component representing a single medicine item, with options to add, remove, delete, and update its count.
+ * @param medName - The name of the medicine.
+ * @param count - The initial count of the medicine.
+ * @param onDelete - Callback function to delete the medicine.
+ * @param onUpdate - Callback function to update the medicine count.
+ */
 
 const Medicine = ({
-  medName,
-  count,
-  onDelete,
-  onUpdate,
+	medName,
+	count,
+	onDelete,
+	onUpdate,
 }: {
-  medName: string;
-  count: number;
-  onDelete: (medName: string) => void;
-  onUpdate: (medName: string, newCount: number) => void;
+	medName: string;
+	count: number;
+	onDelete: (medName: string) => void;
+	onUpdate: (medName: string, newCount: number) => void;
 }) => {
-  const [currCount, setCurrCount] = useState(count);
-  const [modalVisible, setModalVisible] = useState(false);
+	const [currCount, setCurrCount] = useState(count);
+	const [modalVisible, setModalVisible] = useState(false);
 
-  const handleSubtraction = () => {
-    if (currCount > 0) {
-      console.log(`You have ${count} ${medName} left`);
-      const newCount = currCount - 1;
-      setCurrCount(newCount);
-      onUpdate(medName, newCount);
+	const handleSubtraction = () => {
+		if (currCount > 0) {
+			console.log(`You have ${count} ${medName} left`);
+			const newCount = currCount - 1;
+			setCurrCount(newCount);
+			onUpdate(medName, newCount);
 
-      if (currCount - 1 === 0) {
-        Alert.alert("Alert", `Please restock ${medName}`, [{ text: "OK" }]);
-      }
-    }
-  };
+			if (currCount - 1 === 0) {
+				Alert.alert("Alert", `Please restock ${medName}`, [{ text: "OK" }]);
+			}
+		}
+	};
 
-  const handleDeletion = () => {
-    onDelete(medName);
-    setModalVisible(!modalVisible);
-  };
+	const handleDeletion = () => {
+		onDelete(medName);
+		setModalVisible(!modalVisible);
+	};
 
-  const handleAddition = () => {
-    const newCount = currCount + 1;
-    setCurrCount(newCount);
-    onUpdate(medName, newCount);
-    console.log(`You have ${newCount} ${medName} now`);
-  };
+	const handleAddition = () => {
+		const newCount = currCount + 1;
+		setCurrCount(newCount);
+		onUpdate(medName, newCount);
+		console.log(`You have ${newCount} ${medName} now`);
+	};
 
-  return (
-    <View>
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert("modal has been closed.");
-          setModalVisible(!modalVisible);
-        }}
-      >
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>{medName}</Text>
-            <View style={styles.valueButtons}>
-              <Pressable style={styles.increase} onPress={handleAddition}>
-                <Text style={styles.textStyle}>Add {medName}</Text>
-              </Pressable>
+	return (
+		<View>
+			<Modal
+				animationType="slide"
+				transparent={true}
+				visible={modalVisible}
+				onRequestClose={() => {
+					Alert.alert("modal has been closed.");
+					setModalVisible(!modalVisible);
+				}}
+			>
+				<View style={styles.centeredView}>
+					<View style={styles.modalView}>
+						<Text style={styles.modalText}>{medName}</Text>
+						<View style={styles.valueButtons}>
+							<Pressable style={styles.increase} onPress={handleAddition}>
+								<Text style={styles.textStyle}>Add {medName}</Text>
+							</Pressable>
 
-              <Pressable style={styles.decrease} onPress={handleSubtraction}>
-                <Text style={styles.textStyle}>Remove {medName}</Text>
-              </Pressable>
-            </View>
-            <Pressable
-              style={styles.closeButton}
-              onPress={() => handleDeletion()}
-            >
-              <Text style={styles.textStyle}>Delete {medName}</Text>
-            </Pressable>
-            <Pressable
-              style={styles.closeButton}
-              onPress={() => setModalVisible(!modalVisible)}
-            >
-              <Text style={styles.textStyle}>Close</Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
+							<Pressable style={styles.decrease} onPress={handleSubtraction}>
+								<Text style={styles.textStyle}>Remove {medName}</Text>
+							</Pressable>
+						</View>
+						<Pressable
+							style={styles.closeButton}
+							onPress={() => handleDeletion()}
+						>
+							<Text style={styles.textStyle}>Delete {medName}</Text>
+						</Pressable>
+						<Pressable
+							style={styles.closeButton}
+							onPress={() => setModalVisible(!modalVisible)}
+						>
+							<Text style={styles.textStyle}>Close</Text>
+						</Pressable>
+					</View>
+				</View>
+			</Modal>
 
-      <TouchableNativeFeedback
-        onPress={() => {
-          setModalVisible(true);
-        }}
-      >
-        <View style={styles.medContainer}>
-          <View style={styles.medicinePill}>
-            <View style={styles.pill1}></View>
-            <View style={styles.pill2}></View>
-          </View>
+			<TouchableNativeFeedback
+				onPress={() => {
+					setModalVisible(true);
+				}}
+			>
+				<View style={styles.medContainer}>
+					<View style={styles.medicinePill}>
+						<View style={styles.pill1}></View>
+						<View style={styles.pill2}></View>
+					</View>
 
-          <Text style={styles.medTextStyle}>{medName}</Text>
-          <Text style={styles.medCountStyle}>{currCount}</Text>
-        </View>
-      </TouchableNativeFeedback>
-    </View>
-  );
+					<Text style={styles.medTextStyle}>{medName}</Text>
+					<Text style={styles.medCountStyle}>{currCount}</Text>
+				</View>
+			</TouchableNativeFeedback>
+		</View>
+	);
 };
 
 export default Medicine;
 
 const styles = StyleSheet.create({
-  medContainer: {
-    flexDirection: "row",
-    backgroundColor: "white",
-    marginVertical: 5,
-    marginTop: 20,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 10,
-    borderColor: "white",
-    borderWidth: 1,
-  },
-  medicinePill: {
-    flexDirection: "column",
-    height: 25,
-    width: 15,
-    marginRight: 10,
-    borderRadius: 25,
-    transform: [{ rotate: "45deg" }],
-    overflow: "hidden",
-  },
-  pill1: {
-    backgroundColor: "blue",
-    flex: 1,
-  },
-  pill2: {
-    backgroundColor: "red",
-    flex: 1,
-  },
-  medTextStyle: {
-    color: "black",
-    fontSize: 20,
-    width: "80%",
-    paddingLeft: 10,
-  },
-  medCountStyle: {
-    color: "black",
-    fontSize: 20,
-    textAlign: "right",
-    width: "20%",
-    paddingRight: 20,
-  },
-  increase: {
-    backgroundColor: "green",
-    padding: 25,
-    borderRadius: 10,
-    margin: 10,
-    width: "50%",
-  },
-  decrease: {
-    backgroundColor: "red",
-    padding: 25,
-    borderRadius: 10,
-    margin: 10,
-    width: "50%",
-  },
-  centeredView: {
-    flex: 1,
-    marginTop: 22,
-  },
-  modalView: {
-    position: "absolute",
-    bottom: 0,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 30,
-    width: "100%",
-    height: "60%",
-    alignItems: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    fontSize: 30,
-    width: "100%",
-    textAlign: "center",
-  },
-  textStyle: {
-    color: "white",
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-    fontSize: 20,
-  },
-  valueButtons: {
-    flexDirection: "row",
-    marginTop: 20,
-  },
-  closeButton: {
-    backgroundColor: "red",
-    padding: 20,
-    margin: 10,
-    borderRadius: 10,
-    width: "40%",
-  }
+	medContainer: {
+		flexDirection: "row",
+		backgroundColor: "white",
+		marginVertical: 5,
+		marginTop: 20,
+		paddingVertical: 10,
+		paddingHorizontal: 20,
+		borderRadius: 10,
+		borderColor: "white",
+		borderWidth: 1,
+	},
+	medicinePill: {
+		flexDirection: "column",
+		height: 25,
+		width: 15,
+		marginRight: 10,
+		borderRadius: 25,
+		transform: [{ rotate: "45deg" }],
+		overflow: "hidden",
+	},
+	pill1: {
+		backgroundColor: "blue",
+		flex: 1,
+	},
+	pill2: {
+		backgroundColor: "red",
+		flex: 1,
+	},
+	medTextStyle: {
+		color: "black",
+		fontSize: 20,
+		width: "80%",
+		paddingLeft: 10,
+	},
+	medCountStyle: {
+		color: "black",
+		fontSize: 20,
+		textAlign: "right",
+		width: "20%",
+		paddingRight: 20,
+	},
+	increase: {
+		backgroundColor: "green",
+		padding: 25,
+		borderRadius: 10,
+		margin: 10,
+		width: "50%",
+	},
+	decrease: {
+		backgroundColor: "red",
+		padding: 25,
+		borderRadius: 10,
+		margin: 10,
+		width: "50%",
+	},
+	centeredView: {
+		flex: 1,
+		marginTop: 22,
+	},
+	modalView: {
+		position: "absolute",
+		bottom: 0,
+		backgroundColor: "white",
+		borderRadius: 20,
+		padding: 30,
+		width: "100%",
+		height: "60%",
+		alignItems: "center",
+	},
+	modalText: {
+		marginBottom: 15,
+		fontSize: 30,
+		width: "100%",
+		textAlign: "center",
+	},
+	textStyle: {
+		color: "white",
+		textAlign: "center",
+		justifyContent: "center",
+		alignItems: "center",
+		fontSize: 20,
+	},
+	valueButtons: {
+		flexDirection: "row",
+		marginTop: 20,
+	},
+	closeButton: {
+		backgroundColor: "red",
+		padding: 20,
+		margin: 10,
+		borderRadius: 10,
+		width: "40%",
+	}
 });
